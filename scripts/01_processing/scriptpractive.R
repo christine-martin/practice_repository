7db23f93759224e38289e8510c64f013b1bf44d5
Kheely <kimberlyheely@gmail.com>
2025-09-09 09:44:37 -0400

    Added a facet wrap

--- scripts/01_processing/scriptpractive.R
+++ scripts/01_processing/scriptpractive.R
@@ -11,12 +11,13 @@ data("data_heatwaves")
 #build a plot
 ggplot(data = data_heatwaves,
             mapping = aes(x = year, y = temp_mean,
-                          group = paste(fishery, eu_rnpa))) +
-  geom_line()
+                          group = paste(fishery, eu_rnpa), colour = "purple")) +
+  geom_line() +
+  facet_wrap(~ fishery, ncol = 2, scales = "free_y")
 
 p <- ggplot(data = data_heatwaves,
             mapping = aes(x = year, y = temp_mean,
-                          group = paste(fishery, eu_rnpa))) +
+                          group = paste(fishery, eu_rnpa), colour = "orange")) +
   geom_line()
 
 
